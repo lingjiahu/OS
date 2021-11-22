6212cf7e3988c1206468704107a24630fcdb8871
lingjiahu <lingjia.hu@mail.mcgill.ca>
2021-11-22 15:27:10 -0500

    added comments

--- ass/a3/sfs_api.c
+++ ass/a3/sfs_api.c
@@ -1,55 +1,139 @@
+/*
+Implementation of a Simple File System, which can be mounted over an emulated disk system.
+Guaranteed to work in Linux.
+Single level directory - no subdirectories.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
-#define MAXFILENAME 16+1+3 /* max file name + max extension size */
+#define MAXFILENAME 20  // filename (16) + dot (1) + extension (3)
 #define BLOCK_SIZE 1024
 #define MAXFILESIZE (12*BLOCK_SIZE) + ((BLOCK_SIZE/sizeof(int))*BLOCK_SIZE)
 
-// create the file system
-// fresh == 1: new; else: old
+/* 
+format virtual disk & create a file system on top of the virtual disk
+
+input: 
+    fresh: whether file system should be created from scratch
+        fresh == true: new file system
+        fresh == false: open existing file system from disk
+*/
 void mksfs(int fresh)
 { 
 
 }
 
-// 
+/*
+copy the name of the next file in directory to fname
+can be used to loop through a directory
+function remebers current positon in directory at each call TODO: global var?
+
+input:
+    fname: store the name of the next file in directory
+return: non-zero if there is a new file
+        0 if all files have been returned
+*/
 int sfs_getnextfilename(char* fname)
 {
 
 }
 
+/*
+get size of a given file
+
+input: path of a file
+return: size of a given file
+*/
 int sfs_getfilesize(const char* path)
 {
 
 }
 
+/*
+open a file in append mode with file discriptor at the end of the file
+if file does not exits, create a new file with size = 0
+
+input: 
+    name: name of file to open
+return: index of the newly opened file in the file descriptor table
+*/
 int sfs_fopen(char* name)
 {
 
 }
 
-int sfs_fclose(int fildID)
+/*
+close a file by removing its corresponding entry in the file descriptor table
+
+input: 
+    fileID: id of a file in file descriptor table
+return: 
+    0 on success
+    negative value (-1) otherwise
+*/
+int sfs_fclose(int fileID)
 {
 
 }
 
-int sfs_fwrite(int fildID, const char* buf, int length)
+/*
+write buf characters to disk
+write data in buf to file from the current file pointer
+increase the size of the file, the amount of increase depends on the current file pointer
+
+input: 
+    fileID: id of file to write to
+    buf: data to be written
+    length: size of buf
+return: number of bytes written
+*/
+int sfs_fwrite(int fileID, const char* buf, int length)
 {
 
 }
 
-int sfs_fread(int fildID, char* buf, int length)
+/*
+read characters from disk to buf
+TODO: from the current file pointer?
+
+input:
+    fileID: id of file to be read from
+    buf: data to be read to
+    length: size of characters to be read
+return: number of bytes read
+*/
+int sfs_fread(int fileID, char* buf, int length)
 {
 
 }
 
-int sfs_fseek(int fildID, int loc)
+/*
+move read/ write pointer to a given location
+
+input:
+    fileID: id of file to be read from
+    loc: location of relocation
+return:
+    0 on success
+    negative value (-1) otherwise
+*/
+int sfs_fseek(int fileID, int loc)
 {
 
 }
 
+/*
+remove file from directory entry
+release file allocation table entries and data blocks used by the file
+
+input: 
+    file: name of file to be removed
+return:
+    TODO:?
+*/
 int sfs_remove(char* file)
 {
 
